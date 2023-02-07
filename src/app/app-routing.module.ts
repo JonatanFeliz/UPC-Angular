8e20b913138ab7ba4ce5a4803e58857a0ed916ae
Jonatan Feliz <jonatanfelizf@gmail.com>
2023-02-07 13:50:25 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TeamComponent } from './components/team/team.component';
+import { HowtoplayComponent } from './components/howtoplay/howtoplay.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomeComponent,
+  },
+  {
+    path:'play',
+    component: HowtoplayComponent,
+  },
+  {
+    path:'team',
+    component: TeamComponent,
+  },
+  {
+    path:'contact',
+    component: ContactComponent,
+  },
+  {
+    path:'login',
+    component: LoginComponent,
+
+  },
+  {
+    path:'register',
+    component: RegisterComponent,
+
+  },
+  { path: '', 
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', 
+   component: PageNotFoundComponent 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { HowtoplayComponent } from './components/howtoplay/howtoplay.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TeamComponent } from './components/team/team.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    HowtoplayComponent,
+    ContactComponent,
+    TeamComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
