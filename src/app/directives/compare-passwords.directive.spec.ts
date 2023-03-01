df2f563cbc43a9850133be4b92c1891f67232dc5
Jonatan Feliz <jonatanfelizf@gmail.com>
2023-03-01 20:45:00 +0100

    finish

--- src/app/directives/compare-passwords.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ComparePasswordsDirective } from './compare-passwords.directive';
-
-describe('ComparePasswordsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ComparePasswordsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
